1f0055a765fde6bc48efd5e8924b2e389718b133
Océane <120165879+st4rhub@users.noreply.github.com>
2023-03-26 18:27:11 +0200

    Add files via upload

--- Parcelle.java
+++ Parcelle.java
@@ -7,47 +7,96 @@ public class Parcelle{
     private String section;
     private int num_parcelle;
     private double superficie;
-    private ArrayList <Activite_Culture> activites;
+    private List <Activite_Culture> activites;
 
     // Le constructeur
 
-    public Parcelle(String section, int num_parcelle, double superficie, ArrayList <Activite_Culture> activites){
+    public Parcelle(String section, int num_parcelle, double superficie, List <Activite_Culture> activites){
         this.section = section;
         this.num_parcelle = num_parcelle;
         this.superficie = superficie;
         this.activites = activites;
     }
 
-    // Les méthodes :
+    // Exception pour parcelle négative
 
-    public void setNum(int num_parc){ // MARCHE PAS LA CONNE DE SES MORTS
-        try{
-            this.num_parcelle = num_parc;
-        } catch (num_parc < 0){
-            System.out.println("Ne pas mettre de numéro négatif poir les parcelles !");
+    class ParcelleException extends Exception{
+        public ParcelleException(){
+            System.out.println("Ne pas mettre de numéro négatif pour les parcelles !");
         }
     }
 
+    // Getters/Setters :
+
+    public List<Activite_Culture> getActivites(){
+        return this.activites;
+    }
+
+    public void setNum(int num_parc) throws ParcelleException{ // MARCHE PAS LA CONNE DE SES MORTS
+       if (num_parc < 0){
+        throw new ParcelleException();
+       }
+       else{
+        this.num_parcelle = num_parc;
+       }
+    }
+
+    // Méthodes autres :
+
     public double calcul_aide(Activite_Culture act){
-            if (act.getNom() == "recolte"){
+            if (act.getClass().getName()== "Activite_Recolte"){
                 return(1000*this.superficie);
             }
             else{
-                if (act.getNom() == "anti-insectes"){
+                if (act.getClass().getName()== "Activite_Anti_Insectes"){
                 return(2000 + 1000*this.superficie);
                 }
             }
+            return 0;
         }
 
     public double quantite_eau(Activite_Culture act){
-        if (act.getNom() == "recolte"){
+        if (act.getClass().getName()== "Activite_Recolte"){
             return(200*this.superficie);
         }
         else{
-            if (act.getNom() == "anti-insectes"){
+            if (act.getClass().getName()== "Activite_Anti_Insectes"){
                 return(100*this.superficie);
             }
         }
+        return 0;
+    }
+
+    public Activite_Culture cout_max(List<Activite_Culture> activites){
+        int max = 0;
+        List<Integer> semaines = new ArrayList <Integer> ();
+        Activite_Culture act_max = new Activite_Culture("", semaines, 0);
+        for (Activite_Culture act : activites){
+            if (act.getCoutAnnuel() > max){
+                max = act.getCoutAnnuel();
+                act_max = act;
+            }
+        }
+        return act_max;
+    }
+
+    public int nb_activites(String nom_activite){
+        int compteur_activites = 0;
+        if (nom_activite =="Activite_Culture"){
+            compteur_activites = activites.size();
+        }
+        else{
+            for (Activite_Culture act : activites){
+                if (act.getClass().getName() == nom_activite){
+                    compteur_activites += 1;
+                }
+            }
+        }
+        return compteur_activites;
+    }
+
+    public String toString(){
+        return "Parcelle : Section : " + this.section + ", Numéro : " + this.num_parcelle;
     }
 
 }
